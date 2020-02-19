eef3d6bf8361dd4b3c8f23fd0b983ad7686eedcd
Ca7alin-Dumitru <georgecatalin.dumitru@gmail.com>
2020-02-19 14:18:26 +0200

    small updates on cucumber framework

--- seleniumeasy_tests/src/test/java/layer/higher/SeleniumeasyCucumberTests.java
+++ seleniumeasy_tests/src/test/java/layer/higher/SeleniumeasyCucumberTests.java
@@ -7,7 +7,7 @@ import cucumber.api.testng.TestNGCucumberRunner;
 import org.testng.annotations.*;
 
 //@RunWith(Cucumber.class)
-@CucumberOptions(features = {"./../seleniumeasy_cucumber_tests/src/main/java/features"}, glue = {"layer.higher.cucumber.steps"}, plugin = {"pretty"})
+@CucumberOptions(features = {"./../seleniumeasy_cucumber_reader/src/main/java/features"}, glue = {"layer.higher.cucumber.steps"}, plugin = {"pretty"})
 public class SeleniumeasyCucumberTests {
     private TestNGCucumberRunner testNGCucumberRunner;
 
